fa15613e75b1596cc81d46d884a84ac7636961c1
Rohit kumar Rai <rohitrai100191@gmail.com>
2015-09-20 14:56:14 +0530

    hi

---
+++ Collections_Examples/src/collections/map_demo.java
@@ -0,0 +1,39 @@
+package collections;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class map_demo {
+
+	
+	
+	public static void main(String args[])
+	{
+	Map<String,String> map=new HashMap<String, String>();
+		
+		map.put("rohit", "rohit");
+		map.put("rahul", "rohit");
+		map.put("deepak", "rohit");
+		map.put("Saurabh", "rohit");
+	/*	for(Map.Entry<String,String> entry:map.entrySet())
+		{
+			System.out.println(entry.getKey()+"     "+entry.getValue());
+			
+			
+		}
+	*/
+		
+		Set<String> set=new HashSet<String>();
+		
+		
+		
+	}
+		
+
+	
+	
+
+}
